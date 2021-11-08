c10b463e348c1549715a5d6056f04c444e13c4de
thaddeusrayd <thaddeusrayd@gmail.com>
2021-11-08 11:50:55 -0600

    swap functioning

--- src/swap/swap.js
+++ src/swap/swap.js
@@ -4,6 +4,31 @@
  * @param {Node} x
  * @param {Node} y
  */
-function swap(list, x, y) {}
+function swap(list, x, y) {
+  if (!list.head) {
+    return list;
+  }
+
+  let x_next = x.next;
+  let x_prev = list.findWithPrevious((node) => node === x)[1];
+  let y_prev = list.findWithPrevious((node) => node === y)[1];
+
+  x.next = y.next;
+  y.next = x_next;
+
+  if (x_prev) {
+    x_prev.next = y;
+  } else {
+    list.head = y;
+  }
+
+  if (y_prev) {
+    y_prev.next = x;
+  } else {
+    list.head = x;
+  }
+
+  return list;
+}
 
 module.exports = swap;
